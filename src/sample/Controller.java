509deae64423296a8322f1c2a46fefd667d2339c
billytrinh <quanghoa.trinh@gmail.com>
2021-03-20 11:46:20 +0700

    up code

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,16 +3,32 @@ package sample;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+
 public class Controller {
     public TextField txtName;
     public TextField txtEmail;
     public TextArea txtAddress;
     public TextArea ketqua;
 
+    public static ArrayList<String> ds = new ArrayList<>();
+    public static boolean flag = true;
     public void submit(){
         String n = txtName.getText();
         String e = txtEmail.getText();
         String a = txtAddress.getText();
-        ketqua.setText(n+"--"+e+"--"+a);
+        if(!n.isEmpty() && !e.isEmpty() && !a.isEmpty()){
+            ds.add(n+"--"+e+"--"+a+"\n");
+            // sort
+            String txt= "";
+            for(String s:ds){
+                txt+= s;
+            }
+            ketqua.setText(txt);
+            txtName.setText("");
+            txtEmail.setText("");
+            txtAddress.setText("");
+        }
+
     }
 }
